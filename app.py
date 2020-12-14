b08ff654f70386ab35c1fc2bdde5d89c738a682d
jerome cote <jeromecote98@gmail.com>
2020-12-14 16:22:43 -0500

    updated streamlit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from SessionState import get
 import streamlit as st
 from theposturewizard import utils
 from re import search
@@ -14,6 +15,7 @@ APPARATUS = "Bodyweight"
 PART_OF_BODY = "Shoulders"
 
 model = utils.load_my_model(PATH_TO_MODEL)
+
 
 def get_pictures(exercice):
     my_dict = {}
@@ -47,6 +49,7 @@ def info(exercice):
 
     # pictures
     pictures = get_pictures(exercice)
+    i
     for picture in pictures:
         url = 'http://' + picture[2:]
         response = requests.get(url)
@@ -61,15 +64,38 @@ def info(exercice):
         st.video(vid, format='video/m3u8', start_time=0)
         st.write(f"{video}")
 
+def get_input_img():
+    uploaded_file = st.file_uploader("Choose a file", type=['png', 'jpg'])
+
+    if uploaded_file:
+        pos = Image.open(uploaded_file)
+        cols = st.beta_columns(5)
+        with cols[2]:
+            st.image(pos, use_column_width=True)
+    return uploaded_file
+
+
+# STREAMLIT PAGE
+params = {'get_prediction':False, 'file_name':'hello'}
+session_state = get(**params)
+st.write(session_state.file_name)
 
 '''
 # The Posture Wizard
 '''
+
 st.markdown("""Upload your picture""")
-uploaded_file = st.file_uploader("Choose a file", type=['png', 'jpg'])
+
+uploaded_file = get_input_img()
+
+if uploaded_file:
+    if uploaded_file.name != session_state.file_name:
+        session_state.get_prediction = False
+        session_state.file_name = uploaded_file.name
 
 
-if st.button('Get prediction'):
+if st.button('Get prediction') or session_state.get_prediction:
+    session_state.get_prediction = True
 
     if uploaded_file:
 
@@ -77,31 +103,43 @@ if st.button('Get prediction'):
         st.write(f"Your posture: {pred}")
 
         if pred == "kyphosis":
-            stretch, strength = utils.main(APPARATUS, PART_OF_BODY)
+            st.write("**Customize your plan! : **")
 
-            # stretch
-            st.markdown(f"""## 1. Stretching Exercise""")
+            bodypart = st.selectbox('Select a bodypart you want to work on', ['Shoulders', 'Neck'])
 
-            info(stretch[0])
+            if bodypart == 'Shoulders':
+                apparatus = st.selectbox('Select an apparatus', ['Bodyweight', 'Dumbbell', 'Cable'])
 
-            """
+            else:
+                apparatus = 'Bodyweight'
+
+            if st.button('Get your plan!'):
+                #TODO stretch, strength = utils.main(apparatus, bodypart)
+
+                # stretch
+                st.markdown(f"""## 1. Stretching Exercise""")
+
+                info(stretch[0])
+
+                """
 
 
-            """
+                """
 
-            # strength
-            st.markdown(f"""## 2. Strengthening Exercises""")
+                # strength
+                st.markdown(f"""## 2. Strengthening Exercises""")
 
-            # strength 1
-            info(strength[0])
+                # strength 1
+                info(strength[0])
 
-            """
+                """
 
-            """
+                """
 
-            # strength 2
-            info(strength[1])
-
+                # strength 2
+                info(strength[1])
+            else:
+                st.write('Hit `Get your plan` to start working on your posture!')
 
         else:
             st.write("Do you still want to work on your posture?")
